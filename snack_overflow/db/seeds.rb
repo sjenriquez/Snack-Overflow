61c540b32282285ea3fe2b8ee349b7576a306e3f
Jon Campbell <heycampbell@gmail.com>
2015-02-06 13:58:45 -0500

    added image to seed file

--- snack_overflow/db/seeds.rb
+++ snack_overflow/db/seeds.rb
@@ -5,7 +5,7 @@
 25.times {
   votable_type_arr = ["comment", "nom"].sample
   decision_arr = [1, -1].sample
-  Nom.create(user_id: rand(1..12), title: Faker::Lorem.sentence, review: Faker::Lorem.paragraph)
+  Nom.create(user_id: rand(1..12), title: Faker::Lorem.sentence, review: Faker::Lorem.paragraph, pic_url: Faker::Avatar.image)
   Comment.create(user_id: rand(1..12), nom_id: rand(1..25), body: Faker::Lorem.paragraph, parent_id: nil)
   Vote.create(votable_id: rand(1..12), votable_type: votable_type_arr, decision: decision_arr)
 }
