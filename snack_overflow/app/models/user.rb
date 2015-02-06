803c5c3add0958080295e5a6d7b111f007376547
Mai <mxngyn@gmail.com>
2015-02-06 11:55:58 -0500

    Add validation to user fields

--- snack_overflow/app/models/user.rb
+++ snack_overflow/app/models/user.rb
@@ -7,5 +7,15 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+
+  def brownie_points
+
+  end
+
+  def upvoted
+
+  end
 
 end
