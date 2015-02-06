4176debe8e13cb050ca33d553d5a039ddd844d4c
Mai <mxngyn@gmail.com>
2015-02-06 11:55:17 -0500

    Change score to brownie_points

--- snack_overflow/db/migrate/20150206165128_add_details_to_users.rb
+++ snack_overflow/db/migrate/20150206165128_add_details_to_users.rb
@@ -2,7 +2,7 @@ class AddDetailsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :email, :string
     add_column :users, :pic_url, :string
-    add_column :users, :score, :integer
+    add_column :users, :brownie_points, :integer
     add_column :users, :bio, :text
     add_column :users, :website_url, :string
     add_column :users, :name, :string
