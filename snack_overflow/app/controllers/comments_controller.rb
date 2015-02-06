0d706b1727af3d35d3f5b2695149b4fd283dc387
Scott Enriquez <sjenriquez@gmail.com>
2015-02-05 20:17:15 -0500

    Add all CRUD actions to comments controller.

---
+++ snack_overflow/app/controllers/comments_controller.rb
@@ -0,0 +1,53 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @comments = Comments.all
+  end
+
+  def show
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def edit
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      redirect_to :back
+    else
+      @errors = @comment.errors.messages
+      redirect_to :back
+    end
+  end
+
+  def update
+    if @comment.update(comment_params)
+       redirect_to :back
+    else
+      @errors = @comment.errors.messages
+      redirect_to :back
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to :back
+  end
+
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body, :post_id, :parent_id, :user_id)
+  end
+end
