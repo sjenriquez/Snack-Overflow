38863e1ebcc12225f394bb22961eba827a4aca21
Mai <mxngyn@gmail.com>
2015-02-05 20:43:42 -0500

    Change :username to username:

--- snack_overflow/app/controllers/sessions_controller.rb
+++ snack_overflow/app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(:username session_params[:username])
+    @user = User.find_by(username: session_params[:username])
 
     if @user.authenticate(session_params[:password])
       session[:user_id] = @user.id
