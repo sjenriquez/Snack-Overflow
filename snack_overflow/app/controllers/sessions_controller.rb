6ae238a2cf3f215622ed55103e297098868bcc80
Mai <mxngyn@gmail.com>
2015-02-05 20:28:10 -0500

    Add new, create, and destroy controllers for session

--- snack_overflow/app/controllers/sessions_controller.rb
+++ snack_overflow/app/controllers/sessions_controller.rb
@@ -5,11 +5,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(:username session_params[:username])
 
+    if @user.authenticate(session_params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to :back
+    end
   end
 
   def destroy
-
+    reset_session
+    redirect_to root_path
   end
 
   private
