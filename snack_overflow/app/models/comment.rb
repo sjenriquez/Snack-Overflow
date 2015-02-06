ee9d873a96a1545782a5a9a8088a159638814af5
Scott Enriquez <sjenriquez@gmail.com>
2015-02-05 20:21:30 -0500

    Add validations to comment model.

--- snack_overflow/app/models/comment.rb
+++ snack_overflow/app/models/comment.rb
@@ -5,4 +5,8 @@ class Comment < ActiveRecord::Base
 
   has_many :replies, foreign_key: :parent_id, class_name: "Comment"
   belongs_to :parent, foreign_key: :parent_id, class_name: "Comment"
+
+  validates :body, presence: true
+  validates :post_id, presence: true
+  validates :user_id, presence: true
 end
