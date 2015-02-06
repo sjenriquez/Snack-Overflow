b081bee426a26f157f9439c407ed842b51fb3785
Mai <mxngyn@gmail.com>
2015-02-06 15:25:07 -0500

    Test to sign up user successfully

---
+++ snack_overflow/spec/features/users_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature "STUFF" do
+  let(:user) {FactoryGirl.create(:user)}
+
+  scenario "Creates a new user successfully" do
+    visit signup_path
+
+    fill_in "Username", :with => user.username
+    fill_in "Password", :with => user.password
+    fill_in "Password confirmation", :with => user.password
+    fill_in "Email", :with => user.email
+    click_on "Sign Up"
+
+    expect(page).to have_content(user.username)
+  end
+
+end
