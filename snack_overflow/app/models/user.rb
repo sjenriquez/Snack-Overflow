6e2770848aa2ea2df15347744f82c4113dac209b
Mai <mxngyn@gmail.com>
2015-02-05 20:13:49 -0500

    Add validation to user

--- snack_overflow/app/models/user.rb
+++ snack_overflow/app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :votes, as: :votable
 
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
+
 end
