27691d024271a7b60dbe826eabb772755fbc9bad
Mai <mxngyn@gmail.com>
2015-02-06 14:59:55 -0500

    Comments out validation on user for testing

--- snack_overflow/app/models/user.rb
+++ snack_overflow/app/models/user.rb
@@ -5,10 +5,10 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :votes, as: :votable
 
-  validates :username, presence: true, uniqueness: true
-  validates :password, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :name, presence: true
+  # validates :username, presence: true, uniqueness: true
+  # validates :password, presence: true
+  # validates :email, presence: true, uniqueness: true
+  # validates :name, presence: true
 
   def brownie_points
 
