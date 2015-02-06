a1e586a9d63e6de44e6be64051c2d53b8afc8d7e
David Rifkin & Scott Enriquez <pair+dlrifkin.sjenriquez@devbootcamp.com>
2015-02-06 14:59:56 -0500

    Add tests for comment upvotes and downvotes

--- snack_overflow/app/models/comment.rb
+++ snack_overflow/app/models/comment.rb
@@ -11,4 +11,21 @@ class Comment < ActiveRecord::Base
   validates :body, presence: true
   # validates :post_id, presence: true
   # validates :user_id, presence: true
+
+  def upvote
+    Vote.create(votable: self, decision: 1)
+  end
+
+  def downvote
+    Vote.create(votable: self, decision: 0)
+  end
+
+  def upvotes
+    self.votes.where(decision: 1)
+  end
+
+  def downvotes
+    self.votes.where(decision: 0)
+  end
+
 end
