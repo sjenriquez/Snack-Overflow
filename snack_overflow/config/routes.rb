022f61cb7dd4319cc512f86872f5ae48404db977
Mai <mxngyn@gmail.com>
2015-02-05 19:33:45 -0500

    create routes

--- snack_overflow/config/routes.rb
+++ snack_overflow/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/users/:id', to: 'users#show'
+  get '/users/:id/edit', to: 'users#edit'
+  patch '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
+
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   get '/signout', to: 'sessions#destroy'
