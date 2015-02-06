f4e6de47296f1a9379a92abb754c3cca9f7b9061
Mai <mxngyn@gmail.com>
2015-02-06 15:00:45 -0500

    Schema

--- snack_overflow/db/schema.rb
+++ snack_overflow/db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206165128) do
-
+ActiveRecord::Schema.define(version: 20150206165451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,13 +39,6 @@ ActiveRecord::Schema.define(version: 20150206165128) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "pic_url"
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.integer  "post_id"
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "tags", force: :cascade do |t|
