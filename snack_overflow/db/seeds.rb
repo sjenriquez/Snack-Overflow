de870fa131fdb1086c4f54e36b7a1b5f64e466dd
Mai <mxngyn@gmail.com>
2015-02-05 21:43:29 -0500

    Seed data

--- snack_overflow/db/seeds.rb
+++ snack_overflow/db/seeds.rb
@@ -6,6 +6,6 @@
   votable_type_arr = ["comment", "nom"].sample
   decision_arr = [1, -1].sample
   Nom.create(user_id: rand(1..12), title: Faker::Lorem.sentence, review: Faker::Lorem.paragraph)
-  Comment.create(user_id: rand(1..12), post_id: rand(1..25), body: Faker::Lorem.paragraph, parent_id: nil)
+  Comment.create(user_id: rand(1..12), nom_id: rand(1..25), body: Faker::Lorem.paragraph, parent_id: nil)
   Vote.create(votable_id: rand(1..12), votable_type: votable_type_arr, decision: decision_arr)
 }
