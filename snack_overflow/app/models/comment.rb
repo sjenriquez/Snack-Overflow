b16cf80cd514637b8ad16d9fcb2ef43594dc6a4b
Scott Enriquez <sjenriquez@gmail.com>
2015-02-07 15:39:49 -0500

    Remove associations for nested comments from comment model.

--- snack_overflow/app/models/comment.rb
+++ snack_overflow/app/models/comment.rb
@@ -1,13 +1,10 @@
 class Comment < ActiveRecord::Base
-  
+
   include Scoring
 
   belongs_to :user
 
   belongs_to :nom
-
-  has_many :replies, foreign_key: :parent_id, class_name: "Comment"
-  belongs_to :parent, foreign_key: :parent_id, class_name: "Comment"
 
   has_many :votes, as: :votable
 
