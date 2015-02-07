054989be3fa6190b7262beddc490bcaa5ad474e9
Mai <mxngyn@gmail.com>
2015-02-07 18:09:54 -0500

    Add nom_random to welcome controller

--- snack_overflow/app/controllers/welcome_controller.rb
+++ snack_overflow/app/controllers/welcome_controller.rb
@@ -3,5 +3,6 @@ class WelcomeController < ApplicationController
     @noms_most_recent = Nom.most_recent
     @noms_most_popular = Nom.most_popular
     @vending = Nom.vend_ten
+    @nom_random = Nom.rand_nom
   end
 end
