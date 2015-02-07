1d566842cf4c30baa3743d91659ac83aca0ae1a3
Scott Enriquez <sjenriquez@gmail.com>
2015-02-07 16:24:19 -0500

    Remove vote and score methods from Nom model, Scoring module already included.

--- snack_overflow/app/models/nom.rb
+++ snack_overflow/app/models/nom.rb
@@ -23,26 +23,6 @@ class Nom < ActiveRecord::Base
     @noms[0..10]
   end
 
-  def upvote
-    Vote.create(votable: self, decision: 1)
-  end
-
-  def downvote
-    Vote.create(votable: self, decision: 0)
-  end
-
-  def upvotes
-    self.votes.where(decision: 1)
-  end
-
-  def downvotes
-    self.votes.where(decision: 0)
-  end
-
-  def score
-    self.upvotes.count - self.downvotes.count
-  end
-
   def self.rand_nom
     @nom = Nom.all.sample
   end
