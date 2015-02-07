93503baccdd903b370c9efb1100062995fffb21a
Dionne Stanfield <dstanfield.esq@gmail.com>
2015-02-07 11:54:51 -0500

    Make create action for tags and private method for all params

--- snack_overflow/app/controllers/tags_controller.rb
+++ snack_overflow/app/controllers/tags_controller.rb
@@ -1,6 +1,20 @@
 class TagsController < ApplicationController
 
   def create
+    @tag = Tagn.new(tag_parmas)
+
+    if @tag.save
+      redirect_to :index
+    else
+      #change this so that it refers to tags errors
+      @errors = @comment.errors.messages
+      redirect_to :back
+    end
   end
+
+  private
+    def tag_params
+      params.require(:tag).permit(:id, :name, :tagable_id, :tagable_type)
+    end
 
 end
