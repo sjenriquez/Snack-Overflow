a23917c47e06eca50e73b509b1f830ca76328251
Mai <mxngyn@gmail.com>
2015-02-07 18:09:43 -0500

    User authentication for sessions

--- snack_overflow/app/controllers/sessions_controller.rb
+++ snack_overflow/app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(username: session_params[:username])
 
-    if @user.authenticate(session_params[:password])
+    if @user && @user.authenticate(session_params[:password])
       session[:user_id] = @user.id
       redirect_to root_path
     else
